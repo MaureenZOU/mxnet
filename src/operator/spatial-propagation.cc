85cbb8287748a82e9d3da0765b05eacb5b5fd96e
Xueyan Zou <13253956@life.hkbu.edu.hk>
2018-04-27 13:55:45 +0800

    Add files via upload

--- src/operator/spatial-propagation.cc
+++ src/operator/spatial-propagation.cc
@@ -1,13 +1,42 @@
-#include "./spatial-propagation-inl.h"
+/* 
+ * Written by Xueyan Zou @TuSimple Algorithm Intern
+ */
 
+#include "./spatial-propagation-inl.h"
+#include "./mshadow_op.h"
+
+namespace mshadow { 
+template<typename Dtype>
+inline void SPNForward(const Tensor<gpu, 4, Dtype> &data,
+					   const Tensor<gpu, 4, Dtype> &g1,
+					   const Tensor<gpu, 4, Dtype> &g2,
+					   const Tensor<gpu, 4, Dtype> &g3,
+					   const Tensor<gpu, 4, Dtype> &out,
+					   const bool horizontal_,
+					   const bool reverse_){
+	printf("Not implemented");
+}//end SPNForward
+
+template<typename Dtype>
+inline void SPNBackward(const Tensor<gpu, 4, Dtype> &data,
+					    const Tensor<gpu, 4, Dtype> &g1,
+					    const Tensor<gpu, 4, Dtype> &g2,
+					    const Tensor<gpu, 4, Dtype> &g3,
+					    const Tensor<gpu, 4, Dtype> &out,
+					    const Tensor<gpu, 4, Dtype> &data_diff,
+					    const Tensor<gpu, 4, Dtype> &g1_diff,
+					    const Tensor<gpu, 4, Dtype> &g2_diff,
+					    const Tensor<gpu, 4, Dtype> &g3_diff,
+					    const Tensor<gpu, 4, Dtype> &out_diff,
+					    const bool horizontal_,
+					    const bool reverse_){
+	printf("Not implemented");
+}//end SPNBackward
+
+}  // namespace mshadow
 namespace mxnet {
 namespace op {
 template<>
-Operator* CreateOp<cpu>(SpnParam param, int dtype,
-                        std::vector<TShape> *in_shape,
-                        std::vector<TShape> *out_shape,
-                        Context ctx) {
-	LOG(FATAL) << "CPU unimplemented";
-}
-}
+Operator *CreateOp<cpu>(SpnParam param, int dtype) {
+  return new SpnOp<cpu, DType>(param);
 }
